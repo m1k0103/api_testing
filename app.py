6e9c943373929050f1de7d8b6f930d507b777601
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-04-21 20:40:51 +0100

    added dev branch. started user storage]

--- app.py
+++ app.py
@@ -8,7 +8,22 @@ CORS(app)
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "POST":
-        print(request.json)
-        return jsonify({"status":200})
+        data = request.json
+        #basic user data storing tests
+        usrdata = [data["username"], data["password"]]
+        with open("./users.txt", "r") as f:
+            contents = [i.strip("\n").split(":") for i in f.readlines()]
+            for i in range(len(contents)):
+                if usrdata[0] in contents[i]:
+                    if usrdata[1] in contents[i]:
+                        return jsonify({"registered":True}) 
+            return jsonify({"registered":False})
     elif request.method == "GET":
         return render_template("login_page.html")
+    
+@app.route("/signup", route=["GET", "POST"])
+def signup():
+    if request.method == "POST": # TO DO: create signup functionality
+        pass
+    elif request.method == "GET":
+        return render_template("signup.html")
