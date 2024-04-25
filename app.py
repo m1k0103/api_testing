c3d6b9532d5d86f656e504078a00484798d0155c
miki <84585119+miko1034@users.noreply.github.com>
2024-04-25 09:55:09 +0100

    read/write creds to basic text file, edited POST req, testing

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
-import json
 
 def writeData(data):
-    with open("userNpass.txt", "w+") as f:
-        contents = json.load(f)
-        for i in contents["username"]:
-            if i == data["username"]:
-                return True
-        json.dump(data, f, ensure_ascii=False, indent=4)
-        return False
+    to_write = f'{data["username"]}:{data["password"]}'
+    to_write_split = to_write.split(":")
+    with open("userNpass.txt", "r") as f:
+        contents = f.readlines()
+        for i in range(len(contents)):
+            contents[i] = contents[i].replace("\n", "").split(":")
+            if to_write_split[0] not in contents[i][0]:
+                 continue
+            else:
+                 return False
+            
+    with open("userNpass.txt", "a") as f:
+        f.write(f"{to_write}\n")
+        return True
 
+def checkIfUserExists(json):
+    userdata = [json["username"],json["password"]]
+    with open("userNpass.txt", "r") as f:
+        contents = f.readlines()
+        for i in range(len(contents)):
+            contents[i] = contents[i].replace("\n", "").split(":")
+            if userdata[0] in contents[i][0]:
+                 return True
+            else:
+                 return False
 
 app = Flask(__name__)
 CORS(app)
@@ -22,8 +38,13 @@ def homepage():
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "POST":
-        print(request.json)
-        return jsonify({"status":200})
+        if checkIfUserExists(request.json) == True:
+            print(request.json)
+            return jsonify({"status":401, "message":"bad password or user doesnt exist"})
+        elif checkIfUserExists(request.json) == False:
+            return jsonify({"status":200, "message":"logged in"})
+        else:
+            return jsonify({"status":401, "message": "denied"})
     elif request.method == "GET":
         return render_template("login_page.html")
     
@@ -32,9 +53,8 @@ def signup():
     if request.method == "GET":
         return render_template("signup_page.html")
     elif request.method == "POST":
-        a = writeData(request.json)
-        if a == False:
+        if writeData(request.json) == False:
             return jsonify({"status":200, "message":"Signed up successfully"})
-        elif a == True:
+        elif writeData(request.json) == True:
             return jsonify({"status":200, "message":"Username exists"})
 
---
+++ test.py
@@ -0,0 +1,26 @@
+data = {'username': 'test', 'password': '1234'}
+
+def writeData(data):
+    to_write = f'{data["username"]}:{data["password"]}'
+    to_write_split = to_write.split(":")
+    with open("userNpass.txt", "r") as f:
+        contents = f.readlines()
+        for i in range(len(contents)):
+            contents[i] = contents[i].replace("\n", "").split(":")
+            if to_write_split[0] not in contents[i][0]:
+                 continue
+            else:
+                 return {"status":403, "message":"user already registered"}
+    with open("userNpass.txt", "a") as f:
+        f.write(f"{to_write}\n")
+    
+
+def readData(data):
+    with open("userNpass.txt", "r") as f:
+            contents = f.readlines()
+            for i in range(len(contents)):
+                contents[i] = contents[i].replace("\n", "").split(":")
+
+
+
+writeData(data)
